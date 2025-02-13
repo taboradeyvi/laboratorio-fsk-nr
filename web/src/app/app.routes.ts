6af28fec39b1b58472d8b2096ca4361e8ce89641
taboradeyvi <taborapaz@outlook.es>
2025-02-13 08:58:06 -0600

    fix: bug in the route guardian

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -18,6 +18,7 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'home',
--- web/src/app/guards/auth.guard.ts
+++ web/src/app/guards/auth.guard.ts
@@ -10,6 +10,9 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
+@Injectable({
+  providedIn: 'root',
+})
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
@@ -19,10 +22,17 @@ export class AuthGuard implements CanActivate {
   ): Observable<boolean> | Promise<boolean> | boolean {
     const token = localStorage.getItem('token');
     if (token) {
+      if (next.url[0].path === 'login') {
+        this.router.navigate(['/home/patients']);
+        return false;
+      }
       return true;
     } else {
-      this.router.navigate(['/login']);
-      return false;
+      if (next.url[0].path !== 'login') {
+        this.router.navigate(['/login']);
+        return false;
+      }
+      return true;
     }
   }
 }
